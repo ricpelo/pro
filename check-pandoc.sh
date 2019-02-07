07d553301ad8d810f075307a8c6cfecd55b7d386
Ricardo Pérez <ricpelo@gmail.com>
2019-02-07 18:04:07 +0100

    check-pandoc.sh siempre actualiza a la última versión

--- check-pandoc.sh
+++ check-pandoc.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
-VER="2.5"
-FILE="pandoc-$VER-1-amd64.deb"
-
-if ! dpkg -s pandoc > /dev/null 2>&1
-then
-    echo "Descargando e instalando Pandoc $VER..."
+instalar()
+{
     wget -q "https://github.com/jgm/pandoc/releases/download/$VER/$FILE"
     sudo dpkg -i $FILE
     rm -f $FILE
     sudo apt -f install
+}
+
+VER=$(curl --silent "https://api.github.com/repos/jgm/pandoc/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+FILE="pandoc-$VER-1-amd64.deb"
+
+if ! dpkg -s pandoc > /dev/null 2>&1; then
+    echo "Descargando e instalando Pandoc $VER..."
+    instalar
+else
+    ACTUAL=$(pandoc -v | grep -Po 'pandoc \K.*')
+    if [ "$VER" != "$ACTUAL" ]; then
+        echo "Actualizando Pandoc de $ACTUAL a $VER..."
+        instalar
+    fi
 fi
