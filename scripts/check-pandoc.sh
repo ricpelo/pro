bc98393c8cf534dcc5f47cc81c29e20d5ce88619
Ricardo Pérez <ricpelo@gmail.com>
2019-03-21 11:44:34 +0100

    Mejoras en check-pandoc.sh

--- scripts/check-pandoc.sh
+++ scripts/check-pandoc.sh
@@ -2,22 +2,21 @@
 
 instalar()
 {
-    wget -q "https://github.com/jgm/pandoc/releases/download/$VER/$FILE"
-    sudo dpkg -i $FILE
-    rm -f $FILE
-    sudo apt -f install
+    local FILE="pandoc-$1-1-amd64.deb"
+    local TEMP_DEB="$(mktemp)"
+    curl -sL -o $TEMP_DEB "https://github.com/jgm/pandoc/releases/download/$1/$FILE" && sudo dpkg -i $TEMP_DEB && sudo apt -f install
+    rm -f $TEMP_DEB
 }
 
 VER=$(curl --silent "https://api.github.com/repos/jgm/pandoc/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-FILE="pandoc-$VER-1-amd64.deb"
 
 if ! dpkg -s pandoc > /dev/null 2>&1; then
     echo "Descargando e instalando Pandoc $VER..."
-    instalar
+    instalar $VER
 else
     ACTUAL=$(pandoc -v | grep -Po '^pandoc \K.*')
     if [ "$VER" != "$ACTUAL" ]; then
         echo "Actualizando Pandoc de $ACTUAL a $VER..."
-        instalar
+        instalar $VER
     fi
 fi
