55a65898c7d4e68502b89a77b75ed57ebff9580c
Ricardo Pérez <ricpelo@gmail.com>
2019-04-15 09:10:28 +0200

    Añade fecha de modificación de las diapositivas

--- scripts/diapositivas.sh
+++ scripts/diapositivas.sh
@@ -23,9 +23,10 @@ echo "| Título | HTML | PDF | Apuntes |"
 echo "| ------ |:----:|:---:|:-------:|"
 
 for f in $FILES; do
+    FECHA=$(date +'%Y-%m-%d %H:%M:%S' --date @$(stat -c %Y $SOURCE/$f))
     HTML="slides/${f%.md}.html"
     PDF="pdf/${f%.md}.pdf"
     APUNTES="apuntes/${f%.md}-apuntes.pdf"
     TITLE=$(grep -Po "^title: \K.*" $SOURCE/$f)
-    echo "| $TITLE | [HTML]($HTML){:target=\"_blank\"} | [PDF]($PDF){:target=\"_blank\"} | [Apuntes]($APUNTES){:target=\"_blank\"}"
+    echo "| <strong>$TITLE</strong><br><small>($FECHA)</small> | [HTML]($HTML){:target=\"_blank\"} | [PDF]($PDF){:target=\"_blank\"} | [Apuntes]($APUNTES){:target=\"_blank\"}"
 done
