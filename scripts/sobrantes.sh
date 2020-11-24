3b011cd2276ca2891204836115124a3a5fbace0a
Ricardo Pérez López <ricpelo@gmail.com>
2020-11-24 17:32:40 +0100

    Ahora scripts/sobrantes.sh encuentra los que faltan

--- scripts/sobrantes.sh
+++ scripts/sobrantes.sh
@@ -1,5 +1,25 @@
 #!/bin/sh
 
-for p in $1/*; do
-    grep -q "[[({]$(basename $p)[])}]" $2/*.md || echo $p
+a="[[({]"
+z="[])}]"
+
+for archivo in $1/*; do
+    archivo=$(basename -- "$archivo")
+    if ! grep -q "$a$archivo$z" $2/*.md; then
+        nombre="${archivo%.*}"
+        extension="${archivo##*.}"
+        if [ "$extension" = "svg" ] || [ "$extension" = "pdf" ]; then
+            if ! grep -q "$a$nombre.!EXT$z" $2/*.md; then
+                echo $archivo
+            else
+                for ext in svg pdf; do
+                    if [ ! -f "$1/$nombre.$ext" ]; then
+                        echo "*** Falta: $nombre.$ext ***"
+                    fi
+                done
+            fi
+        else
+            echo $archivo
+        fi
+    fi
 done
